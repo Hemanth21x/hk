1ee8d95aabdc598bf055eac1a261d3d43cd1ef5e
Hemanth21x <hemanthkumar5980@gmail.com>
2026-04-30 12:24:45 +0530

    operators

---
+++ src/operators/Arthematic.java
@@ -0,0 +1,35 @@
+package operators;
+
+public class Arthematic {
+	
+	int add(int a,int b) {
+	 return a+b;	
+	}
+	int sub(int a,int b) {
+	 return a-b;		
+	}
+	int mul(int a,int b) {
+		return a*b;	
+	}
+	int div(int a,int b) {
+		return a/b;	
+	}
+	int mod(int a,int b) {
+		return a%b;	
+	}
+	
+	void show() {
+		System.out.println("add : "+add(19,45));
+		System.out.println("sub : "+sub(16,17));
+		System.out.println("mul : "+mul(65,78));
+		System.out.println("div : "+div(34,76));
+		System.out.println("mod : "+mod(65,84));
+		}
+
+	public static void main(String[] args) {
+		Arthematic ref = new Arthematic();
+		ref.show();
+
+	}
+
+}
---
+++ src/operators/Swap.java
@@ -0,0 +1,26 @@
+package operators;
+
+public class Swap {
+	
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=5; 
+		
+		a=a+b;
+		b=a-b;
+		System.out.println("b: "+b);
+		a= a-b;
+		System.out.println("a: "+a);
+		//int temp=a;
+		//System.out.println(temp);
+		//a=b;
+		//System.out.println(a);
+		
+		//b=temp;
+		//System.out.println(b);
+		
+	
+		}
+
+}
