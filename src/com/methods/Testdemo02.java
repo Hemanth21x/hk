a17a59ed01a95c7cbc1280ecea399af207dc1320
Hemanth21x <hemanthkumar5980@gmail.com>
2026-07-07 22:45:18 +0530

    o1

---
+++ src/com/methods/Testdemo02.java
@@ -0,0 +1,19 @@
+package com.methods;
+
+//Add Two Numbers Using a Method
+
+public class Testdemo02 {
+	
+	public int mul(int a) {
+		return a*a;
+	}
+
+	public static void main(String[] args) {
+		Testdemo02 ref = new Testdemo02();
+		
+		System.out.println("addtion : "+ref.mul(10));
+		
+
+	}
+
+}
