4e3cd56338f494a33f7acc8076b22a50562200bc
Hemanth21x <hemanthkumar5980@gmail.com>
2026-07-10 15:33:55 +0530

    Collection02

---
+++ src/com/collections/TestDemo02.java
@@ -0,0 +1,22 @@
+package com.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TestDemo02 {
+	
+	public static void main (String [] args) {
+		
+		Collection <String> cs = new ArrayList <>();
+		
+		cs.add("Hemanth");
+		cs.add("Baji");
+		cs.add("durga Prasd");
+		cs.add("Rakesh");
+		cs.add("Gowtham");
+		
+		System.out.println("Students"+cs);
+		
+	}
+
+}
