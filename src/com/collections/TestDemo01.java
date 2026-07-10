4fcd7f96982d697da5840a2be7e32a8e752ef34c
Hemanth21x <hemanthkumar5980@gmail.com>
2026-07-10 15:43:02 +0530

    coolection 01

---
+++ src/com/collections/TestDemo01.java
@@ -0,0 +1,21 @@
+package com.collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class TestDemo01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a string : ");
+		String str=sc.nextLine();
+		
+		char [] ch=str.toCharArray();
+		Map<Character,integer> m
+		
+
+	}
+
+}
