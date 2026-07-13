3b207e318a4b2df915d1f2391ba361d914fe9274
Hemanth21x <hemanthkumar5980@gmail.com>
2026-07-13 15:37:03 +0530

    commit

---
+++ src/com/opps2/SortedArray.java
@@ -0,0 +1,21 @@
+package com.opps2;
+
+public class SortedArray {
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 4, 7, 11, 15 };
+		int target = 15;
+		int temp = 0;
+
+		for (int i = 0; i < arr.length-1; i++) {
+			int j = i + 1;
+//		    do {
+//		    	
+//		    }while(arr[i]+arr[j]== target) {
+//		    	  System.out.println();
+//		    }
+
+		}
+	}
+
+}
